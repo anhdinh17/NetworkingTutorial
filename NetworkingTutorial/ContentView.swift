9e402cfc8b0072b430743c156c725258bf0366f5
Anh Dinh <anhdinh17@gmail.com>
2024-11-22 00:16:28 -0600

    Initial Commit

---
+++ NetworkingTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingTutorial
+//
+//  Created by Anh Dinh on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Anh Dinh on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
